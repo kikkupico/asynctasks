1924de8b2ccfaa8792e181264fe521144428d7b5
vramakin <vramak@gmail.com>
2017-04-18 13:43:25 +0530

    Added setup.py

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(name='asynctasks',
+      version='0.1',
+      description='Asynchronous task scheduler and runner for Python',
+      url='https://github.com/vramakin/asynctasks',
+      author='Ram',
+      author_email='vramak@gmail.com',
+      license='MIT',
+      packages=['asynctasks'],
+      zip_safe=False)
